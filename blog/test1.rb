2c022d6489c50be4fc70b88e0fe9eab6142fa982
James O'Neal <jamesponeal@gmail.com>
2015-12-20 12:50:39 -0600

    update to website blog

--- blog/test1.rb
+++ blog/test1.rb
@@ -1,24 +1,39 @@
 
 
-hash1 = (1..6).group_by { |i| i%3 }
-puts hash1
+class Pet
 
-
-test_array = [1,2,3,4,5,6]
-hash2 = test_array.group_by{|x| x > 3}
-puts hash2
-
-name_array = ["Jim", "Bob", "Kate", "Dave", "James", "Thomas", "Carrie", "Rumpelstiltskin"]
-hash3 = name_array.group_by{|x| x.length}
-puts hash3
-
-food_array = ["Carrot Cake", "Steak", "Pumpkin Pie", "Blueberry Pie", "Hamburger", "Pizza", "Chocolate Cake", "Salad"]
-hash4 = food_array.group_by do |x|
-  if x.include?("Cake") || x.include?("Pie")
-    "Dessert"
-  else
-    "Dinner"
+  def initialize
+    @happiness = 5
+    @hunger = 5
+    @energy = 5
+    @alive_status = true
   end
+
+  def walk
+    @happiness += 1
+    @hunger += 1
+    @energy -= 1
+    still_alive?
+  end
+
+  def feed
+    @hunger -= 2
+    @happiness += 1
+    still_alive?
+  end
+
+  def put_to_bed
+    @hunger += 1
+    @energy += 2
+    @happiness -= 1
+    still_alive?
+  end
+
+  def still_alive?
+    @alive_status = false if @happiness <= 0
+    @alive_status = false if @hunger >= 10
+    @alive_status = false if @energy <= 0
+  end
+
 end
-puts hash4
 
