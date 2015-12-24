53caaf1c3e20a031319a964ffaf5d5e5f3621914
James O'Neal <jamesponeal@gmail.com>
2015-12-24 15:02:58 -0600

    update blog

--- blog/test1.rb
+++ blog/test1.rb
@@ -1,39 +1,43 @@
 
 
-class Pet
+# class Pet
 
-  def initialize
-    @happiness = 5
-    @hunger = 5
-    @energy = 5
-    @alive_status = true
-  end
+#   def initialize
+#     @happiness = 5
+#     @hunger = 5
+#     @energy = 5
+#     @alive_status = true
+#   end
 
-  def walk
-    @happiness += 1
-    @hunger += 1
-    @energy -= 1
-    still_alive?
-  end
+#   def walk
+#     @happiness += 1
+#     @hunger += 1
+#     @energy -= 1
+#     still_alive?
+#   end
 
-  def feed
-    @hunger -= 2
-    @happiness += 1
-    still_alive?
-  end
+#   def feed
+#     @hunger -= 2
+#     @happiness += 1
+#     still_alive?
+#   end
 
-  def put_to_bed
-    @hunger += 1
-    @energy += 2
-    @happiness -= 1
-    still_alive?
-  end
+#   def put_to_bed
+#     @hunger += 1
+#     @energy += 2
+#     @happiness -= 1
+#     still_alive?
+#   end
 
-  def still_alive?
-    @alive_status = false if @happiness <= 0
-    @alive_status = false if @hunger >= 10
-    @alive_status = false if @energy <= 0
-  end
+#   def still_alive?
+#     @alive_status = false if @happiness <= 0
+#     @alive_status = false if @hunger >= 10
+#     @alive_status = false if @energy <= 0
+#   end
 
+# end
+
+number = 10
+for i in 1..number
+  puts "My favorite number is #{i}."
 end
-
